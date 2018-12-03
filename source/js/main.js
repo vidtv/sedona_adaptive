c29e806bef724cfea74fd25fde315418eedcdec7
vidtv <nikit1533@yandex.ru>
2018-12-04 02:08:39 +0300

    Fixed mobile burger menu

--- source/js/main.js
+++ source/js/main.js
@@ -1,16 +1,13 @@
-let mobileMenuButton = document.querySelector(".main-nav__open");
+let mobileMenuButtonOpen = document.querySelector(".main-nav__open");
+let mobileMenuButtonClose = document.querySelector(".site-list__close");
 let mobileMenu = document.querySelector(".main-nav");
 
-mobileMenuButton.addEventListener("click", function () {
-    if (mobileMenuButton.classList.contains("main-nav__open")) {
-        mobileMenu.classList.remove("main-nav__hidden");
-        mobileMenuButton.classList.remove("main-nav__open");
-        mobileMenuButton.classList.add("main-nav__close");
-    } else {
-        mobileMenu.classList.add("main-nav__hidden");
-        mobileMenuButton.classList.remove("main-nav__close");
-        mobileMenuButton.classList.add("main-nav__open");
-    }
+mobileMenuButtonOpen.addEventListener("click", function () {
+    mobileMenu.classList.remove("main-nav__hidden");
+});
+
+mobileMenuButtonClose.addEventListener("click", function () {
+    mobileMenu.classList.add("main-nav__hidden");
 });
 
 
