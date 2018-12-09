3b768cbfbce8524a6a5df045fb9fd489abd38b45
vidtv <nikit1533@yandex.ru>
2018-12-09 15:59:39 +0300

    Popups' locations and opening/closing were stylized

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,14 @@
 let mobileMenuButtonOpen = document.querySelector(".main-nav__open");
 let mobileMenuButtonClose = document.querySelector(".site-list__close");
 let mobileMenu = document.querySelector(".main-nav");
+let popupSuccess = document.querySelector(".popup--success");
+let popupSuccessButton = document.querySelector(".popup__close--success");
+let sedonaForm = document.querySelector(".feedback");
+let formSendButton = document.querySelector(".feedback__send");
+let inputFieldPhone = document.getElementById("feedback__input--phone");
+let inputFieldEmail = document.getElementById("feedback__input--email");
+let inputFieldName = document.getElementById("feedback__input--name");
+let inputFieldSurname = document.getElementById("feedback__input--surname");
 
 mobileMenuButtonOpen.addEventListener("click", function () {
     mobileMenu.classList.remove("main-nav__hidden");
@@ -9,6 +17,19 @@ mobileMenuButtonOpen.addEventListener("click", function () {
 mobileMenuButtonClose.addEventListener("click", function () {
     mobileMenu.classList.add("main-nav__hidden");
 });
+
+sedonaForm.addEventListener("submit", function () {
+    event.preventDefault();
+    popupSuccess.classList.add("visible");
+});
+
+popupSuccessButton.addEventListener("click", function () {
+    popupSuccess.classList.remove("visible");
+    sedonaForm.submit();
+});
+
+
+
 
 
 
